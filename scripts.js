e7dc3665c7644afc045c53e925bcd21ad04833c0
okonnert <64486394+okonnert@users.noreply.github.com>
2020-04-30 17:31:19 +0200

    Added $(selector), toggleClass, etc.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
+//Second part:
 console.log('Scrip file is loaded.');
+
+//First part:
+//Developer Console
+//Statement
+//Numbers
+//Numeric operators (+, -, *, /)
+//Variables
+//Strings
+//Booleans and operators (&&, ||)
+//Arrays
+//Objects
+//Examples:
+//1+1
+//colors
+//name
+//etc.
+
+//Third part:
+$('h1').html('This is done by <em>JavaScript</em>!');
+$ ('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass('blue');
